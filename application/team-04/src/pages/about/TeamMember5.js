862a2b6b3409af860fd1b576dcbb53ce45824721
Om Dankhara <79533535+odankhrara@users.noreply.github.com>
2023-09-13 21:51:35 -0700

    Update TeamMember5.js

--- application/team-04/src/pages/about/TeamMember5.js
+++ application/team-04/src/pages/about/TeamMember5.js
@@ -7,7 +7,7 @@ function TeamMember5() {
         <h1 className="about-title">Om Dankhara</h1>
         <img className="about-image" src="/path/to/om-photo.jpg" alt="Om's Photo" />
         <p className="about-bio">
-        I am an undergraduate major in Computer Scince at San Francisco State University. I am a strong willed and dedicated individual who likes taking on challenges and approaching them with unique ways. I am highly skilled in problem-solving techniques, which drives me towards solving scientific problems. My logical and critical thinking approach has directed me into pursuing Computer Science, where I can efficiently apply my mental skills.
+        I am an undergraduate majoring in Computer Science at San Francisco State University. I am a strong willed and dedicated individual who likes taking on challenges and approaching them with unique ways. I am highly skilled in problem-solving techniques, which drives me towards solving scientific problems. My logical and critical thinking approach has directed me into pursuing Computer Science, where I can efficiently apply my mental skills.
         </p>
     </div>
     );
