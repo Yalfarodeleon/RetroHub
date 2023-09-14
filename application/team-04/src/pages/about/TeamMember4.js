a8fe2e473edc687ccbbaba7ebd2f516ffba99f36
ddhorajiya <79803233+ddhorajiya@users.noreply.github.com>
2023-09-13 20:14:17 -0700

    Update TeamMember4.js

--- application/team-04/src/pages/about/TeamMember4.js
+++ application/team-04/src/pages/about/TeamMember4.js
@@ -7,7 +7,7 @@ function TeamMember4() {
         <h1 className="about-title">Deep Dhorajiya</h1>
         <img className="about-image" src="/path/to/deep-photo.jpg" alt="Deep's Photo" />
         <p className="about-bio">
-        Insert Deep's biography or fun facts here.
+        I am a passionate Computer Science student at San Francisco State University. With a solid foundation in programming languages and data structures, I am constantly exploring new technologies and designing software applications. I am highly skilled in problem-solving, and I enjoy working collaboratively in a team environment
         </p>
     </div>
     );
